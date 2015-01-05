f325417391baece97776af6416ec574540fa9045
wasnot <a.aida@freebit.net>
2015-01-06 01:51:04 +0900

    modified app ui

--- SwiftWidgetTest/TodayViewController.swift
+++ SwiftWidgetTest/TodayViewController.swift
@@ -11,7 +11,7 @@ import NotificationCenter
 import SystemConfiguration
 
 class TodayViewController: UIViewController, NCWidgetProviding {
-        
+    
     @IBOutlet weak var label: UILabel!
     
     override func viewDidLoad() {
@@ -26,12 +26,12 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
         // Perform any setup necessary in order to update the view.
-
+        
         // If an error is encountered, use NCUpdateResult.Failed
         // If there's no update required, use NCUpdateResult.NoData
         // If there's an update, use NCUpdateResult.NewData
-
-//        completionHandler(NCUpdateResult.NewData)
+        
+        //        completionHandler(NCUpdateResult.NewData)
         println("widget update")
         
         self.updateLabel()
@@ -41,58 +41,70 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     func updateLabel()
     {
         println("widget update")
-
-        var dateFormatter = NSDateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
-//        self.label.text = dateFormatter.stringFromDate(NSDate())
+        
+        //        var dateFormatter = NSDateFormatter()
+        //        dateFormatter.dateFormat = "yyyy-MM-dd hh:mm:ss"
+        //        self.label.text = dateFormatter.stringFromDate(NSDate())
         
         var success = false
         
         // 3G接続の場合はnilが戻されるので、以降のコードで注意する。
-//        CFArrayRef
+        //        CFArrayRef
         var interface : Array = CNCopySupportedInterfaces().takeRetainedValue()
         
         if(interface.count>0){
-//            var intArray : Array = interface.takeRetainedValue()
+            //            var intArray : Array = interface.takeRetainedValue()
             var value : String = interface[0] as String
             println("value:"+value)
             
             //        CFDictionaryRef
             println("dicRef,")
-                        var dicRef = CNCopyCurrentNetworkInfo(value)
-//            println(dicRef)
+            var dicRef = CNCopyCurrentNetworkInfo(value)
+            //            println(dicRef)
             if(dicRef==nil){
+                self.label.text = "network infoない"
+                success = true
             }else{
-            println("dic,")
-            var dic : Dictionary = dicRef.takeRetainedValue()
-            println(dic)
-
-            if(dic.count>0){
-                println(dic.count)
-                var ssid : String? = dic[kCNNetworkInfoKeySSID] as? String
-                if(ssid != nil){
-                    println(ssid)
-                    self.label.text = ssid
+                println("dic,")
+                var dic : Dictionary = dicRef.takeRetainedValue()
+                println(dic)
+                
+                if(dic.count>0){
+                    println(dic.count)
+                    var ssid : String? = dic[kCNNetworkInfoKeySSID] as? String
+                    if(ssid != nil){
+                        println(ssid)
+                        self.label.text = ssid
+                        success = true
+                    } else {
+                        self.label.text = "ssidない"
+                        success = true
+                    }
+                    //        var dicRef = CNCopyCurrentNetworkInfo(CFArrayGetValueAtIndex(interface, 0) as CFStringRef);
+                    
+                    //        if (dicRef) {
+                    //            ssid = CFDictionaryGetValue(dicRef, kCNNetworkInfoKeySSID);
+                    // Macアドレスを取得
+                    //macAddress = CFDictionaryGetValue(dicRef, kCNNetworkInfoKeyBSSID);
+                    
+                    //            NSLog(@"%@", ssid);
+                    //        }
+                }else{
+                    self.label.text = "wifiつながってない"
                     success = true
                 }
-                //        var dicRef = CNCopyCurrentNetworkInfo(CFArrayGetValueAtIndex(interface, 0) as CFStringRef);
-                
-                //        if (dicRef) {
-                //            ssid = CFDictionaryGetValue(dicRef, kCNNetworkInfoKeySSID);
-                // Macアドレスを取得
-                //macAddress = CFDictionaryGetValue(dicRef, kCNNetworkInfoKeyBSSID);
-                
-                //            NSLog(@"%@", ssid);
-                //        }
             }
-            }
+        }else{
+            self.label.text = "interfaceない"
+            success = true
         }
         
         if(!success){
-            self.label.text = dateFormatter.stringFromDate(NSDate())
+            //            self.label.text = dateFormatter.stringFromDate(NSDate())
+            self.label.text = "確認中"
         }
     }
-
+    
     @IBAction func buttonTouched(sender: AnyObject) {
         self.updateLabel()
     }
